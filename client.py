cc7b166aed4c46a4c2643b5f2e9b550e3a3389be
Alexey <4pok45@mail.ru>
2018-08-10 15:21:03 +0300

    начал писать клиент

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+import time
+
+class ClientError(Exception):
+    pass
+
+class Client:
+    def __init__(self, host, port, timeout=None):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+        self.sock = socket.create_connection((self.host, self.port), self.timeout)
+
+    def put(self, metric, value, timestamp=None):
+        try:
+            if timestamp == None:
+                self.sock.sendall("put {metr} {val} {time}\n".format(metr=metric, val=str(value), time=str(int(time.time()))).encode("utf-8"))
+            else:
+                self.sock.sendall("put {metr} {val} {time}\n".format(metr=metric, val=str(value), time=str(timestamp)).encode("utf-8"))
+        except socket.error as err:
+            raise ClientError
+
+    def get(self, metric):
+        try:
+            return self.sock.sendall("get {metr}\n".format(metr=metric).encode("utf-8"))
+        except socket.error as err:
+                raise ClientError("error send", err)
